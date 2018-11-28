d69d4115456249ad14ee9d84f9cd7509a11d42be
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-11-28 07:24:28 -0500

    Try stack deletion through CI

--- travis-ci/select-stack.js
+++ travis-ci/select-stack.js
@@ -7,42 +7,44 @@ const git = require('simple-git');
 function determineIntegrationTestStackName(cb) {
   const branch = process.env.TRAVIS_PULL_REQUEST_BRANCH || process.env.TRAVIS_BRANCH;
 
-  if (!branch) return cb('none');
+  return cb('cumulus-nightly');
+
+  // if (!branch) return cb('none');
 
   // Nightly cron job
-  if (process.env.TRAVIS_EVENT_TYPE === 'cron') return cb('cumulus-nightly');
+  // if (process.env.TRAVIS_EVENT_TYPE === 'cron') return cb('cumulus-nightly');
 
-  if (branch === 'master') return cb('cumulus-from-source');
+  // if (branch === 'master') return cb('cumulus-from-source');
 
-  const stacks = {
-    'Aimee Barciauskas': 'aimee-test',
-    'Alireza': 'aj',
-    'Jenny Liu': 'jl',
-    jennyhliu: 'jl',
-    kkelly51: 'kk-uat-deployment',
-    'Lauren Frederick': 'lf-int-test',
-    laurenfrederick: 'lf-int-test',
-    'Mark Boyd': 'mboyd-int',
-    Marc: 'mth-2',
-    yjpa7145: 'mth-2',
-    mhuffnagle: 'mth-2',
-    'Marc Huffnagle': 'mth-2',
-    'Matt Savoie': 'mhs',
-    'Jonathan Kovarik': 'jk',
-    'Menno Van Diermen': 'mvd',
-    'Jacob Campbell': 'jc'
-  };
+  // const stacks = {
+  //   'Aimee Barciauskas': 'aimee-test',
+  //   'Alireza': 'aj',
+  //   'Jenny Liu': 'jl',
+  //   jennyhliu: 'jl',
+  //   kkelly51: 'kk-uat-deployment',
+  //   'Lauren Frederick': 'lf-int-test',
+  //   laurenfrederick: 'lf-int-test',
+  //   'Mark Boyd': 'mboyd-int',
+  //   Marc: 'mth-2',
+  //   yjpa7145: 'mth-2',
+  //   mhuffnagle: 'mth-2',
+  //   'Marc Huffnagle': 'mth-2',
+  //   'Matt Savoie': 'mhs',
+  //   'Jonathan Kovarik': 'jk',
+  //   'Menno Van Diermen': 'mvd',
+  //   'Jacob Campbell': 'jc'
+  // };
 
-  return git('.').log({ '--max-count': '1' }, (e, r) => {
-    const author = r.latest.author_name;
+  // return git('.').log({ '--max-count': '1' }, (e, r) => {
+  //   const author = r.latest.author_name;
 
-    console.error(`Selecting build stack based on author name: "${author}"`);
+  //   console.error(`Selecting build stack based on author name: "${author}"`);
 
-    if (author && stacks[author]) {
-      return cb(stacks[author]);
-    }
-    return cb('cumulus-from-pr');
-  });
+  //   if (author && stacks[author]) {
+  //     return cb(stacks[author]);
+  //   }
+  //   return cb('cumulus-from-pr');
+  // });
 }
 
 determineIntegrationTestStackName(console.log);
